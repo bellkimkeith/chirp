454b08e94e84ad80b127bb092864f7c5bcc258e9
bellkimkeith <157334752+bellkimkeith@users.noreply.github.com>
2024-05-23 11:26:05 +0800

    update eas

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,19 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["react-native-reanimated/plugin"],
+    plugins: [
+      "expo-image-picker",
+      {
+        photosPermission:
+          "The app accesses your photos to let you share them with your friends.",
+      },
+      "expo-media-library",
+      {
+        photosPermission: "Allow $(PRODUCT_NAME) to access your photos.",
+        savePhotosPermission: "Allow $(PRODUCT_NAME) to save photos.",
+        isAccessMediaLocationEnabled: true,
+      },
+      "react-native-reanimated/plugin",
+    ],
   };
 };
